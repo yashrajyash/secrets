512a0ea1a0802aafb12d4f6aff79d55419c54668
yashrajyash <yashrajyash18@gmail.com>
2021-06-26 19:55:37 +0530

    files initialized

---
+++ app.js
@@ -0,0 +1,100 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter password']
+    }
+});
+
+const User = new mongoose.model('User', userSchema);
+
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register', function(req, res) {
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err) {
+            if(!err) {
+                res.render('secrets');
+            } else {
+                console.log(err);
+            }
+        });
+
+    });
+});
+
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true) {
+                        res.render('secrets');
+                    }
+                });
+            }
+        }
+    });
+});
+
+
+
+
+app.listen(port, function() {
+    console.log('Server has started at http://localhost:'+port);
+});
